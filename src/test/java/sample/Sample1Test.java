2e67ff7e17b6cf8a2bfa064327ba1cbb9d966438
gaurav7838433475 <gauravkumar92.badal@gmail.com>
2022-12-05 16:23:08 +0530

    new test

---
+++ src/test/java/sample/Sample1Test.java
@@ -0,0 +1,18 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void tc1()
+	{
+		System.out.println("sript1");
+	}
+	
+	@Test
+	public void tc2()
+	{
+		System.out.println("sript2");
+	}
+}
---
+++ src/test/java/sample/Sample2Test.java
@@ -0,0 +1,18 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+
+	@Test
+	public void tc3()
+	{
+		System.out.println("sript3");
+	}
+	
+	@Test
+	public void tc()
+	{
+		System.out.println("sript4");
+	}
+}
